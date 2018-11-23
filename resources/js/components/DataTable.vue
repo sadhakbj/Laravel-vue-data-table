f0cde1600aa170f6e2eecd3618164d25889d10b8
sadhakbj <sadhakbj@gmail.com>
2018-11-23 11:09:01 +0545

    Episode-2 - [x] implemented sorting, pagination functionality

--- resources/js/components/DataTable.vue
+++ resources/js/components/DataTable.vue
@@ -1,26 +1,46 @@
 <template>
   <div class="data-table">
-    <table class="table">
-      <thead>
-      <tr>
-        <th class="table-head">#</th>
-        <th v-for="column in columns" :key="column"
-            class="table-head">
-          {{ column | columnHead }}
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr class="" v-if="tableData.length === 0">
-        <td class="lead text-center" :colspan="columns.length + 1">No data found.</td>
-      </tr>
-      <tr v-for="(data, key1) in tableData" :key="data.id" class="m-datatable__row" v-else>
-        <td>{{ serialNumber(key1) }}</td>
-        <td v-for="(value, key) in data">{{ value }}</td>
-      </tr>
-      </tbody>
-
-    </table>
+    <div class="main-table">
+      <table class="table">
+        <thead>
+        <tr>
+          <th class="table-head">#</th>
+          <th v-for="column in columns" :key="column" @click="sortByColumn(column)"
+              class="table-head">
+            {{ column | columnHead }}
+            <span v-if="column === sortedColumn">
+                            <i v-if="order === 'asc' " class="fas fa-arrow-up"></i>
+                            <i v-else class="fas fa-arrow-down"></i>
+            </span>
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="" v-if="tableData.length === 0">
+          <td class="lead text-center" :colspan="columns.length + 1">No data found.</td>
+        </tr>
+        <tr v-for="(data, key1) in tableData" :key="data.id" class="m-datatable__row" v-else>
+          <td>{{ serialNumber(key1) }}</td>
+          <td v-for="(value, key) in data">{{ value }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <nav v-if="pagination && tableData.length > 0">
+      <ul class="pagination">
+        <li class="page-item" :class="{'disabled' : currentPage === 1}">
+          <a class="page-link" href="#" @click.prevent="changePage(currentPage - 1)">Previous</a>
+        </li>
+        <li v-for="page in pagesNumber" class="page-item"
+            :class="{'active': page == pagination.meta.current_page}">
+          <a href="javascript:void(0)" @click.prevent="changePage(page)" class="page-link">{{ page }}</a>
+        </li>
+        <li class="page-item" :class="{'disabled': currentPage === pagination.meta.last_page }">
+          <a class="page-link" href="#" @click.prevent="changePage(currentPage + 1)">Next</a>
+        </li>
+        <span style="margin-top: 8px;"> &nbsp; <i>Displaying {{ pagination.data.length }} of {{ pagination.meta.total }} entries.</i></span>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -32,26 +52,94 @@ export default {
   },
   data() {
     return {
-      tableData: []
+      tableData: [],
+      url: '',
+      pagination: {
+        meta: { to: 1, from: 1 }
+      },
+      offset: 4,
+      currentPage: 1,
+      perPage: 5,
+      sortedColumn: this.columns[0],
+      order: 'asc'
+    }
+  },
+  watch: {
+    fetchUrl: {
+      handler: function(fetchUrl) {
+        this.url = fetchUrl
+      },
+      immediate: true
     }
   },
   created() {
-    return this.fetchData(this.fetchUrl)
+    return this.fetchData()
+  },
+  computed: {
+    /**
+     * Get the pages number array for displaying in the pagination.
+     * */
+    pagesNumber() {
+      if (!this.pagination.meta.to) {
+        return []
+      }
+      let from = this.pagination.meta.current_page - this.offset
+      if (from < 1) {
+        from = 1
+      }
+      let to = from + (this.offset * 2)
+      if (to >= this.pagination.meta.last_page) {
+        to = this.pagination.meta.last_page
+      }
+      let pagesArray = []
+      for (let page = from; page <= to; page++) {
+        pagesArray.push(page)
+      }
+      return pagesArray
+    },
+    /**
+     * Get the total data displayed in the current page.
+     * */
+    totalData() {
+      return (this.pagination.meta.to - this.pagination.meta.from) + 1
+    }
   },
   methods: {
-    fetchData(url) {
-      axios.get(url)
-        .then(data => {
-          this.tableData = data.data.data
-        })
+    fetchData() {
+      let dataFetchUrl = `${this.url}?page=${this.currentPage}&column=${this.sortedColumn}&order=${this.order}&per_page=${this.perPage}`
+      axios.get(dataFetchUrl)
+        .then(({ data }) => {
+          this.pagination = data
+          this.tableData = data.data
+        }).catch(error => this.tableData = [])
     },
     /**
      * Get the serial number.
      * @param key
      * */
     serialNumber(key) {
-      return key + 1;
+      return (this.currentPage - 1) * this.perPage + 1 + key
     },
+    /**
+     * Change the page.
+     * @param pageNumber
+     */
+    changePage(pageNumber) {
+      this.currentPage = pageNumber
+      this.fetchData()
+    },
+    /**
+     * Sort the data by column.
+     * */
+    sortByColumn(column) {
+      if (column === this.sortedColumn) {
+        this.order = (this.order === 'asc') ? 'desc' : 'asc'
+      } else {
+        this.sortedColumn = column
+        this.order = 'asc'
+      }
+      this.fetchData()
+    }
   },
   filters: {
     columnHead(value) {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,12 @@
 	
 	<title>Laravel</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 <body>
 <div class="flex-center position-ref full-height" id="app">
-	<div class="container">
+	<div class="container mt-md-5">
+		<h1 class="mb-lg-5">List of all users.</h1>
 		<data-table
 			fetch-url="{{ route('users.table') }}"
 			:columns="['name', 'email', 'address' , 'created_at']"
